b4b9c9049f9220f4705595563c683e2aa0f4bb40
kaise0011 <k.tateno.sys19@morijyobi.ac.jp>
2020-06-18 11:43:13 +0900

    index

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+$dsn = 'mysql:dbname=sample_db;host=localhost;';
+$user = 'tateno';
+$password = 'Ttntennis1123';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+
+    $sql = "select * from user";
+    $result = $dbh ->query($sql);
+
+    echo "接続成功\n";
+} catch (PDOException $e) {
+    echo "接続失敗: " . $e->getMessage() . "\n";
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "container">
+        <?php foreach( $result as $value ){
+            echo "<h1>$value[id] $value[name] $value[age]</h1>";
+        }
+        ?>
+    </div>
+</body>
+</html>
